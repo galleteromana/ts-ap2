6de7a8494e98211bab8577ac84df620e34f29129
Romana ૮꒰ ˶• ༝ •˶꒱ა <159267216+galleteromana@users.noreply.github.com>
2026-04-18 09:15:18 -0300

    6. Classes TestLoader e TestRunner

--- framework.py
+++ framework.py
@@ -64,19 +64,29 @@ class TestSuite:
 
 
 class TestLoader:
+
+    TEST_METHOD_PREFIX = 'test'
+
     def get_test_case_names(self, test_case_class):
-        pass
+        methods = dir(test_case_class)
+        return list(filter(lambda m: m.startswith(self.TEST_METHOD_PREFIX), methods))
 
     def make_suite(self, test_case_class):
-        pass
+        suite = TestSuite()
+        for name in self.get_test_case_names(test_case_class):
+            suite.add_test(test_case_class(name))
+        return suite
 
 
 class TestRunner:
+
     def __init__(self):
         self.result = TestResult()
 
     def run(self, test):
-        pass
+        test.run(self.result)
+        print(self.result.summary())
+        return self.result
 
 
 class TestStub(TestCase):
@@ -169,19 +179,42 @@ class MyTest(TestCase):
         raise Exception()
 
 
-result = TestResult()
-suite = TestSuite()
+class TestLoaderTest(TestCase):
 
-suite.add_test(TestCaseTest('test_result_success_run'))
-suite.add_test(TestCaseTest('test_result_failure_run'))
-suite.add_test(TestCaseTest('test_result_error_run'))
-suite.add_test(TestCaseTest('test_result_multiple_run'))
+    def test_create_suite(self):
+        loader = TestLoader()
+        suite = loader.make_suite(TestStub)
+        assert len(suite.tests) == 3
 
-suite.add_test(TestSuiteTest('test_suite_size'))
-suite.add_test(TestSuiteTest('test_suite_success_run'))
-suite.add_test(TestSuiteTest('test_suite_multiple_run'))
+    def test_create_suite_of_suites(self):
+        loader = TestLoader()
+        stub_suite = loader.make_suite(TestStub)
+        spy_suite = loader.make_suite(TestSpy)
 
-suite.run(result)
-print(result.summary())
+        suite = TestSuite()
+        suite.add_test(stub_suite)
+        suite.add_test(spy_suite)
 
-print(result.summary())
+        assert len(suite.tests) == 2
+
+    def test_get_multiple_test_case_names(self):
+        loader = TestLoader()
+        names = loader.get_test_case_names(TestStub)
+        assert names == ['test_error', 'test_failure', 'test_success']
+
+    def test_get_no_test_case_names(self):
+
+        class Test(TestCase):
+            def foobar(self):
+                pass
+
+        loader = TestLoader()
+        names = loader.get_test_case_names(Test)
+        assert names == []
+
+
+loader = TestLoader()
+suite = loader.make_suite(TestLoaderTest)
+
+runner = TestRunner()
+runner.run(suite)
